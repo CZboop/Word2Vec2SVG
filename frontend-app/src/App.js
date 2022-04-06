cf9a9f47d102da78392a443a1a0554996cfe07b1
Branchey <70140890+CZboop@users.noreply.github.com>
2022-04-06 07:37:08 +0100

    added some styling and made react app show two distinct words for comparison

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import DisplayWordSVG from './components/DisplayWordSVG';
-import data from './data/scp_word_polygon_keyvals_medium.json';
+import data from './data/word_polygon_data_complex.json';
 import {useState} from 'react';
 
 function App() {
@@ -12,26 +12,39 @@ function App() {
     return stored;
   }
 
-  const handleSubmit = () => {
-    console.log(word)
-    sessionStorage.setItem('currentWord', word)
+  const handleSubmit1 = () => {
+    console.log(word1)
+    sessionStorage.setItem('currentWord1', word1)
 }
 
-const [word, setWord] = useState(getSessionStorageOrDefault('currentWord', "example"));
-const wordPoints = word ? data[word]: null;
-console.log(wordPoints)
+const handleSubmit2 = () => {
+  console.log(word2)
+  sessionStorage.setItem('currentWord2', word2)
+}
+
+const [word1, setWord1] = useState(getSessionStorageOrDefault('currentWord1', "example"));
+const wordPoints1 = word1 ? data[word1]: null;
+console.log(wordPoints1)
+
+const [word2, setWord2] = useState(getSessionStorageOrDefault('currentWord2', "example"));
+const wordPoints2 = word2 ? data[word2]: null;
 
   return (
     <>
     <div className="App">
       <header className="App-header">
         <h1>Word2Vec2SVG</h1>
-        <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-           <polygon points={wordPoints}
-              fill="green" stroke="white" strokeWidth="0.05" />
-  </svg>
       </header>
-      <DisplayWordSVG word={word} handleSubmit={handleSubmit} setWord={setWord} />
+      <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+           <polygon points={wordPoints1}
+              fill="rgb(106, 255, 106)"  />
+      </svg>
+      <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+           <polygon points={wordPoints2}
+              fill="rgb(106, 255, 106)"  />
+      </svg>
+      <DisplayWordSVG word={word1} handleSubmit={handleSubmit1} setWord={setWord1} />
+      <DisplayWordSVG word={word2} handleSubmit={handleSubmit2} setWord={setWord2} />
     </div>
     </>
   );
